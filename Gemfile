b5e60589279d5d3c093a2a75ba4940b603dd6d44
potapovDim <dmytrop@weblium.com>
2017-03-22 13:48:51 +0200

    initial

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "rake"
+gem "selenium-webdriver"
+gem 'chromedriver-helper'
+gem 'watir-webdriver', '>=0.9.9'
+gem 'watir', '>=6.2.0'
+gem "rspec-core", "~> 3.4.4"
+gem "rspec"
+gem "parallel"
+gem "browserstack-local"
---
+++ spec/try_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../scripts/browserstack.rb'
+
+describe "Google's Search Functionality" do
+  it "can find search results" do
+    @browser.goto "https://www.google.com/ncr"
+    element = @browser.element(:name, 'q')
+    element.send_keys "BrowserStack"
+    element.onsubmit
+    sleep 5
+    expect(@browser.title).to eql("BrowserStack - Google Search")
+  end
+end
