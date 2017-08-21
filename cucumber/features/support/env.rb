7494a5b53cabde6f9ec9e59669873018fc18ec74
potapovDim <potapovd3012@gmail.com>
2017-08-21 16:01:12 +0300

    add cucumber

---
+++ cucumber/features/support/env.rb
@@ -0,0 +1,51 @@
+require 'watir'
+require 'cucumber'
+
+Before do |scenario|
+  def assert_it message, &block
+    begin
+      if (block.call)
+        puts "Assertion PASSED for #{message}"
+      else
+        puts "Assertion FAILED for #{message}"
+        fail('Test Failure on assertion')
+      end
+    rescue => e
+      puts "Assertion FAILED for #{message} with exception '#{e}'"
+      fail('Test Failure on assertion')
+    end
+  end
+  p "Starting #{scenario}"
+
+  if ENV['REMOTE']
+    b_name = ENV['BROWSER'].downcase 
+    print b_name
+    case b_name
+      when 'firefox'
+        @caps = Selenium::WebDriver::Remote::Capabilities.firefox(
+          :javascript_enabled => true,
+          :marionette => true)
+      when 'chrome'
+        @caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+          :javascript_enabled => true,
+          :marionette => true)
+    end
+    @browser = Watir::Browser.new(
+      :remote,
+      :url => "http://127.0.0.1:4444/wd/hub",
+      :desired_capabilities => @caps) 
+  else
+    @browser = Watir::Browser.new :chrome   
+  end
+
+end
+After do |scenario|
+  if scenario.failed?
+    Dir::mkdir('screenshots') if not File.directory?('screenshots')
+    screenshot = "./screenshots/FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
+    @browser.driver.save_screenshot(screenshot)
+    embed screenshot, 'image/png'
+  end
+  @browser.close
+end
+
---
+++ cucumber/features/test.feature
@@ -0,0 +1,18 @@
+Feature: Amazon Search functionality
+
+
+Scenario: Users can search for a specific item on Amazon
+  Given a user goes to Amazon
+  When they search for "pens"
+  Then amazon should return results for "pens"
+
+Scenario Outline: Users search for a variety of items
+  Given a user goes to Amazon
+  When the user looks for <query> and 
+  Then the results returned will display <query>
+
+  Examples:
+  |query  |
+  |laptop |
+  |guitar |
+  |pants  |
